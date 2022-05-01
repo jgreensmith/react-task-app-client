05dbf0f5bb8c85a0b0236a1b360b72c314dd3889
James Greensmith <jgreensmith13@gmail.com>
2022-05-01 21:41:44 +0100

    auth sign in

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,12 +1,62 @@
-import React from 'react';
+import { Button, Container, FormControl, InputLabel, MenuItem, Paper, Select, TextField, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { InputContainer } from '../utils/styles';
 import Layout from "./Layout";
 
 
 const Auth = () => {
+  const [authForm, setAuthForm] = useState({
+    username: '',
+    password: ''
+  });
+
+  const handleSubmit = () => {
+
+  }
+  const handleChange = () => {
+
+  }
   return (
       <Layout>
+        <Container sx={{ display: "flex", justifyContent: "space-around", alignContent: 'center' }}>
 
-          <div>Auth</div>
+        <Paper  sx={{width: "373px" }}>
+            <div style={{m: 0, p: 2, display: "flex", justifyContent: "space-between"}}>
+                <Typography variant="h6" align="center">Sign In</Typography>
+                
+            </div>
+
+                <form autoComplete="off" noValidate onSubmit={handleSubmit}>
+            
+                    <InputContainer>
+                        <TextField 
+                            name="title"
+                            value={authForm.username}
+                            variant="outlined"
+                            label="Title"
+                            fullWidth
+                            sx={{m:2, pr:3}}
+                            onChange={handleChange}
+                            />
+                    </InputContainer>
+                    <InputContainer>
+                        <TextField
+                            name="message"
+                            value={authForm.password}
+                            variant="outlined"
+                            label="Message"
+                            fullWidth
+                            sx={{m:2, pr:3}}
+                            onChange={handleChange}
+                            />
+                    </InputContainer>
+                    
+
+                
+                    <Button  variant="contained" color="primary" size="large" type="submit" fullWidth sx={{p:2}}>Submit</Button>
+                </form>
+        </Paper>
+        </Container>
       </Layout>
   )
 }
